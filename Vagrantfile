f33d81498fcee9538574d084111a690f2ada8f46
Bartul Bonacic <bartul.bonacic@live.com>
2015-09-30 19:52:54 +0400

    better handling of boxes

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,11 @@
 Vagrant.configure(2) do |config|
   config.vm.hostname = "starter"
 
-  config.vm.box = "box-cutter/ubuntu1404-desktop"
+  config.vm.box = "ubuntu/vivid64" # Official Ubuntu Server 15.04 (Vivid Vervet)
+  config.vm.provider "parallels" do |p, override|
+    override.vm.box = "parallels/ubuntu-14.04"
+  end
+
   config.vm.provision "shell" do |s|
     s.path = "Vagrantprovision-runtime.sh"
   end
--- Vagrantprovision-usertools.sh
+++ Vagrantprovision-usertools.sh
@@ -9,13 +9,13 @@ dnvm install latest -f -y
 dnvm install latest -f -y -r coreclr
 
 # Install atom
-echo "##### Install atom #####"
-curl -sSL https://github.com/atom/atom/releases/download/v1.0.19/atom-amd64.deb > ~/atom-amd64.deb
-sudo dpkg --install ~/atom-amd64.deb
-rm ~/atom-amd64.deb
-apm upgrade
+# echo "##### Install atom #####"
+# curl -sSL https://github.com/atom/atom/releases/download/v1.0.19/atom-amd64.deb > ~/atom-amd64.deb
+# sudo dpkg --install ~/atom-amd64.deb
+# rm ~/atom-amd64.deb
+# apm upgrade
 
 # Install atom addins
-echo "##### Install atom addins #####"
-apm install omnisharp-atom
+# echo "##### Install atom addins #####"
+# apm install omnisharp-atom
 # apm install ionide-installer
