8b72415db78c32aeccf3e092365c2c139f03fe54
Bartul Bonacic <bartul.bonacic@live.com>
2015-10-10 14:18:22 +0400

    dev env first draft

--- dev/Vagrantprovision-runtime.sh
+++ dev/Vagrantprovision-runtime.sh
@@ -7,15 +7,14 @@ sudo apt-get install git -y
 sudo apt-get install unzip -y
 sudo apt-get install curl -y
 
-# Install mono
+echo "##### Install mono #####"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 sudo apt-get update
 sudo apt-get install mono-complete -y
 
 
-# Install libtool
-echo "##### Install libtool #####"
+echo "##### Install libuv #####"
 sudo apt-get install -y automake libtool
 curl -sSL https://github.com/libuv/libuv/archive/v1.4.2.tar.gz | tar zxfv - -C /usr/local/src
 cd /usr/local/src/libuv-1.4.2
